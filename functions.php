cd36a3ef74bad3d6d8be2e258b3d60dca38a7e7b
Andrew McDougal <andrew@ontrendwebsites.com>
2018-02-27 22:11:17 +1100

    homepage: products accordion done. testimonials done. not checked responsive.

--- functions.php
+++ functions.php
@@ -13,86 +13,76 @@ function storefront_homepage_content() {
 	while ( have_posts() ) {
 		the_post(); ?>
 <div class="entry-content">
-	<p>Delivery Notes: For <strong>FREE delivery</strong> in Melbourne metro area a minimum order quantity of <strong>50 grams</strong> is required (caviar &amp; truffles).<br /></p>
-	<p>Chilled Aus Post shipping to Country Victoria or interstate to be quoted.</p>
+	<div class="delivery-notes">
+		<p>Delivery Notes: For <strong>FREE delivery</strong> in Melbourne metro area a minimum order quantity of <strong>50 grams</strong> is required (caviar &amp; truffles).<br /></p>
+		<p>Chilled Aus Post shipping to Country Victoria or interstate to be quoted.</p>
+	</div>
 
-	<div class="accordion-container">
-		<?php $the_query = new WP_Query( 'page_id=26' ); ?>
-		<?php while ($the_query -> have_posts()) : $the_query -> the_post();  ?>
-            
-        <?php 
-        if( have_rows('products') ): 
-        // count for dynamic accordion header link
-        $count = 0;
-        while( have_rows('products') ): the_row();
-        $count++;
-        ?>
-        <!-- accordion header link -->
-        <h4>
-        	<a href="#accordion<?php echo $count; ?>" aria-expanded="false" aria-controls="accordion<?php echo $count; ?>" class="accordion-title">
-        		<?php the_sub_field('simple_product_title'); ?>
-        	</a>
-        </h4>
-        <!-- accordion header link -->
-
-        <!-- accordion content -->
-        <div class="accordion-content" id="accordion<?php echo $count; ?>" aria-hidden="true">
-			<?php  if( have_rows('product_price') ): while( have_rows('product_price') ): the_row();
-			echo '<div class="product-price"><p class="product">'; the_sub_field('product'); echo '</p>';
-			echo '<p class="price">'; the_sub_field('price'); echo '</p></div>';
-			endwhile; endif;
-			?>
+	<div class="accordion-wrapper">
+		<div class="accordion-open">
+			<p>view products &amp; pricing <i class="fa fa-angle-down"></i></p>
 		</div>
-		<!-- accordion content -->
 
-		<?php endwhile; endif; ?>
-     <?php endwhile; ?>
-     <?php wp_reset_query(); ?>
- 	</div>
+		<div class="accordion-container">
+			<?php $the_query = new WP_Query( 'page_id=26' ); ?>
+			<?php while ($the_query -> have_posts()) : $the_query -> the_post();  ?>
+	            
+	        <?php 
+	        if( have_rows('products') ): 
+	        // count for dynamic accordion header link
+	        $count = 0;
+	        while( have_rows('products') ): the_row();
+	        $count++;
+	        ?>
+	        <!-- accordion header link -->
+	        <h4>
+	        	<a href="#accordion<?php echo $count; ?>" aria-expanded="false" aria-controls="accordion<?php echo $count; ?>" class="accordion-title">
+	        		<i class="fa fa-angle-down"></i>
+	        		<?php the_sub_field('simple_product_title'); ?>
+	        	</a>
+	        </h4>
+	        <!-- accordion header link -->
+
+	        <!-- accordion content -->
+	        <div class="accordion-content" id="accordion<?php echo $count; ?>" aria-hidden="true">
+				<?php  if( have_rows('product_price') ): while( have_rows('product_price') ): the_row();
+				echo '<div class="product-price"><p class="product">'; the_sub_field('product'); echo '</p>';
+				echo '<p class="price">'; the_sub_field('price'); echo '</p></div>';
+				endwhile; endif;
+				?>
+			</div>
+			<!-- accordion content -->
+
+			<?php endwhile; endif; ?>
+	     <?php endwhile; ?>
+	     <?php wp_reset_query(); ?>
+	 	</div>
+	 </div>
  </div>
 
 
-		<?php
+<?php get_template_part( 'content', 'homepage' ); ?>
 
-		get_template_part( 'content', 'homepage' );
-
+<!-- testimonials -->
+<div class="entry-content testimonials">
+	<h3 class="feedback">Customer feedback...</h3>
+	<?php if( have_rows('testimonials') ): ?>
+	<ul class="testimonial-slides">
+	<?php while( have_rows('testimonials') ): the_row(); 
+		// vars
+		$words = get_sub_field('words');
+		$name = get_sub_field('name');
 		?>
-
-
-
-			<div class="entry-content">
-
-
-
-
-			
-			
-				<!-- Testimonials -->
-				<h3 class="feedback">Customer feedback</h3>
-				<?php if( have_rows('testimonials') ): ?>
-				<ul class="slides testimonials">
-				<?php while( have_rows('testimonials') ): the_row(); 
-					// vars
-					$words = get_sub_field('words');
-					$name = get_sub_field('name');
-					?>
-					<li class="slide">
-						<h4><?php echo $words; ?></h4>
-						<p><?php echo $name; ?></p>
-					</li>
-				<?php endwhile; ?>
-				</ul>
-				<?php endif; ?>
-				<!-- Testimonials -->
-
-
-
-
-			</div>
-
-
-		<?php
-
+		<li class="slide">
+			<h4><?php echo $words; ?></h4>
+			<p><?php echo $name; ?></p>
+		</li>
+	<?php endwhile; ?>
+	</ul>
+	<?php endif; ?>
+</div>
+<!-- testimonials -->
+<?php
 	} // end of the loop.
 }
 
--- library/js/custom.js
+++ library/js/custom.js
@@ -5,7 +5,15 @@
 	}
 
 	function homeAccordion() {
-		$( ".accordion-container" ).accordion({
+		var $accordionContainer = $('.accordion-container');
+		var $accordionOpen = $('.accordion-open');
+
+		// open accordion container
+		$accordionOpen.click(function() {
+			$accordionContainer.slideToggle('slow');
+		});
+
+		$(".accordion-container").accordion({
 			collapsible: true,
 			active: 'none',
 			heightStyle: 'content'
@@ -46,6 +54,16 @@
 		});
 	}
 
+	function homeTestimonials() {
+		$('.testimonial-slides').slick({
+			arrows: false,
+			dots: true,
+			adaptiveHeight: true,
+			autoplay: true,
+			autoplaySpeed: 7000,
+		});
+	}
+
 
 	
 	function questionsAccordion() {
@@ -80,6 +98,7 @@
 	$(window).on('load', function() {
 		//init();
 		homeAccordion();
+		setTimeout(homeTestimonials, 200);
 		setTimeout(homeslides, 200);
 		setTimeout(shopNewsShow, 5000);
 	});
