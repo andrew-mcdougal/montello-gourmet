808315d212e050fd831c8909b7e13b12ee585cbb
Andrew McDougal <andrew.mcdougal@powershop.com.au>
2021-05-11 21:49:46 +1000

    Header tightened design

--- functions.php
+++ functions.php
@@ -89,14 +89,7 @@ function enfold_customization_extra_product_content() {
 
 // Custom header html using this tutorial: https://wpthememakeover.com/2016/02/15/how-to-change-footer-text-in-the-storefront-theme-for-woocommerce/
 function storefront_site_branding() { ?>
-<div class="site-branding">
-	<div class="beta site-title">
-		<a href="<?php echo home_url(); ?>" rel="nofollow">Montello Gourmet</a>
-	</div>
-	<div class="site-description">
-		<p>Online Caviar store</p>
-		<p>Melbourne based supply, Australia wide delivery.</p>
-	</div>
-</div>
+
 <?php
 }
+
--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 ?><!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	
+  
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -18,7 +18,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-MFXZ5HC');</script>
 <!-- End Google Tag Manager -->
-	
+  
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
 <link rel="profile" href="http://gmpg.org/xfn/11">
@@ -43,7 +43,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 </head>
 
 <body <?php body_class(); ?>>
-	
+  
 <!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MFXZ5HC"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
@@ -52,60 +52,52 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <?php do_action( 'storefront_before_site' ); ?>
 
 <div id="page" class="hfeed site">
-	<?php do_action( 'storefront_before_header' ); ?>
+  <?php do_action( 'storefront_before_header' ); ?>
 
-	<header id="masthead" class="site-header" role="banner" style="<?php storefront_header_styles(); ?>">
-		<div class="col-full">
+  <header id="masthead" class="site-header" role="banner" style="<?php storefront_header_styles(); ?>">
+    <div class="col-full o-grid-layout o-grid-layout--columns-two">
+      <div class="site-branding">
+        <div class="beta site-title">
+          <a href="<?php echo home_url(); ?>" rel="nofollow">Montello Gourmet</a>
+        </div>
+        <div class="site-description">
+          <p>Online Caviar store</p>
+          <p>Melbourne based supply, Australia wide delivery.</p>
+        </div>
+      </div>
 
-			<?php
-			/**
-			 * Functions hooked into storefront_header action
-			 *
-			 * @hooked storefront_skip_links                       - 0
-			 * @hooked storefront_social_icons                     - 10
-			 * @hooked storefront_site_branding                    - 20
-			 * @hooked storefront_secondary_navigation             - 30
-			 * @hooked storefront_product_search                   - 40
-			 * @hooked storefront_primary_navigation_wrapper       - 42
-			 * @hooked storefront_primary_navigation               - 50
-			 * @hooked storefront_header_cart                      - 60
-			 * @hooked storefront_primary_navigation_wrapper_close - 68
-			 */
-			do_action( 'storefront_header' ); ?>
+      <div class="header-right">
+        <p>
+        Mobile: <a href="tel:61416218147" target="blank">+61 416 218 147</a><br>
+        Email: <a href="mailto:tony@montellogourmet.com?subject=Enquiry from website">tony@montellogourmet.com</a>
+      </p>
+        <?php //get_search_form(); ?>
+      </div>
+    </div>
 
-		</div>
+    <?php wp_nav_menu( 
+            array( 
+              'theme_location' => 'primary',
+            ) 
+          ); ?>
 
-		<!-- search bar and contact links -->
-		<div class="col-full searchbar grid-columns-three">
-			<?php //get_search_form(); ?>
-			<div>
-				<p>
-					Mobile: <a href="tel:61416218147" target="blank">+61 416 218 147</a><br>
-					Email: <a href="mailto:tony@montellogourmet.com?subject=Enquiry from website">tony@montellogourmet.com</a>
-				</p>
-			</div>
-		</div>
-		<div class="caviar-shop-button">
-			<a href="<?php echo get_home_url(); ?>/product-category/caviar" onclick="_gaq.push(['_trackEvent', 'Header', 'Clicked', 'Buy Caviar slidedown button', 1]);">Buy Caviar</a>
-		</div>
+  </header><!-- #masthead -->
 
-	</header><!-- #masthead -->
+  <?php
+  /**
+   * Functions hooked in to storefront_before_content
+   *
+   * @hooked storefront_header_widget_region - 10
+   */
+  do_action( 'storefront_before_content' ); ?>
 
-	<?php
-	/**
-	 * Functions hooked in to storefront_before_content
-	 *
-	 * @hooked storefront_header_widget_region - 10
-	 */
-	do_action( 'storefront_before_content' ); ?>
+  <div id="content" class="site-content" tabindex="-1">
+    <div class="col-full">
 
-	<div id="content" class="site-content" tabindex="-1">
-		<div class="col-full">
-
-		<?php
-		/**
-		 * Functions hooked in to storefront_content_top
-		 *
-		 * @hooked woocommerce_breadcrumb - 10
-		 */
-		do_action( 'storefront_content_top' );
+    <?php
+    /**
+     * Functions hooked in to storefront_content_top
+     *
+     * @hooked woocommerce_breadcrumb - 10
+     */
+    do_action( 'storefront_content_top' );
